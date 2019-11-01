ed6a5e8520ab2835b36d8ea509d6b5b4b7c94519
Michael Vega <ichigo509@yahoo.com>
2019-11-01 16:07:34 -0400

    created folder structure and added Nav and Footer

---
+++ portfolio/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Nav from "./Components/Nav/nav";
+import Footer from "./Components/Footer/footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio/src/Components/Footer/footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles.css";
+
+function Footer() {
+    return (
+        <div>
+            <div className="footer">
+                <h4>Footer</h4>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
