b88f15087eb0353e169d65191959122eb2c1a6c9
Michael Vega <ichigo509@yahoo.com>
2019-11-20 03:57:02 -0500

    finished projects page for now, and started contact page

---
+++ portfolio/client/build/precache-manifest.9875935a1c6504aff740dbe00bf20500.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5ce42209f40563249148",
+    "url": "/static/js/main.9e6129ac.chunk.js"
+  },
+  {
+    "revision": "ccedb8919a789ef3a3b8",
+    "url": "/static/js/2.ec770713.chunk.js"
+  },
+  {
+    "revision": "5ce42209f40563249148",
+    "url": "/static/css/main.ebbb14cf.chunk.css"
+  },
+  {
+    "revision": "ccedb8919a789ef3a3b8",
+    "url": "/static/css/2.43db4988.chunk.css"
+  },
+  {
+    "revision": "605c2027df93ee2a696ffafa31e01458",
+    "url": "/index.html"
+  }
+];
--- portfolio/client/src/App.js
+++ portfolio/client/src/App.js
@@ -6,6 +6,7 @@ import AwesomeSlider from 'react-awesome-slider';
 import AwesomeSliderStyles from 'react-awesome-slider/src/styled/cube-animation';
 import "./Components/carousel.scss";
 import Landing from "./Components/Landing/landing";
+import pic from "./Images/pr3Cap.PNG"
 import Sidebar from "./Components/Sidebar/sidebar";
 
 class App extends Component {
@@ -22,21 +23,83 @@ class App extends Component {
 
             <div className="about slide">
               <h2 className="header">About Me</h2>
+              <div className="myPic"></div>
+              <div className="summary"></div>
+              <div className="techSkills"></div>
             </div>
 
             <div className="projects slide">
               <h2 className="header">Projects</h2>
+              <div className="bounty hover-shadow pr">
+                <a href="https://github.com/MV5174/Project3" target="blank">
+                  <div className="pop">
+                    <h4>Bounty Hunter</h4>
+                    <p>A user authenticated video game that relies on React-Redux</p>
+                  </div>
+                </a>
+              </div>
+              <div className="kodex hover-shadow pr">
+                <a href="https://github.com/MV5174/Project2" target="blank">
+                  <div className="pop">
+                    <h4>K()dex</h4>
+                    <p>A user authenticated social media platform for coders</p>
+                  </div>
+                </a>
+              </div>
+              <div className="ez hover-shadow pr">
+                <a href="https://github.com/MV5174/ProjectOne" target="blank">
+                  <div className="pop">
+                    <h4>EZ-Eatz</h4>
+                    <p>An API-based app that finds recipes for ingredients you have or locations serving what you crave</p>
+                  </div>
+                </a>
+              </div>
+              <div className="friend hover-shadow pr">
+                <a href="https://github.com/MV5174/FriendFinder" target="blank">
+                  <div className="pop">
+                    <h4>Friend Finder</h4>
+                    <p>A short survey that matches you with friends</p>
+                  </div>
+                </a>
+              </div>
+              <div className="train hover-shadow pr">
+                <a href="https://github.com/MV5174/Train-Time-HW" target="blank">
+                  <div className="pop">
+                    <h4>Train Scheduler</h4>
+                    <p>An app that uses Firebase and MomentJs to store train schedules for user convenience</p>
+                  </div>
+                </a>
+              </div>
+              <div className="bamazon hover-shadow pr">
+                <a href="https://github.com/MV5174/Bamazon" target="blank">
+                  <div className="pop">
+                    <h4>Bamazon</h4>
+                    <p>A NodeJs-based app that utilizes MYSQL to store data for work convenience</p>
+                  </div>
+                </a>
+              </div>
             </div>
 
             <div className="contact slide">
               <h2 className="header">Contact</h2>
+              <div className="info">
+                <h4>Email: MVega7971@gmail.com </h4>
+                <hr></hr>
+                <h4>Tel: (757) 362-1157</h4>
+                <hr></hr>
+                <h4>Location: Kissimmee, Florida, 34744</h4>
+                <hr></hr>
+                <h4>Github: <a href="https://github.com/MV5174" target="blank">https://github.com/MV5174</a></h4>
+                <hr></hr>
+                <h4>LinkedIn: <a href="https://www.linkedin.com/in/michael-vega-45200b187/" target="blank">https://www.linkedin.com/in/michael-vega-45200b187/</a></h4>
+              </div>
             </div>
 
           </AwesomeSlider>
 
         </div> {/*Ending of the carousel */}
-        
-      {/*Ending of the app */}</div>
+
+        {/*Ending of the app */}</div>
     );
   }
 }
