8ef5261473a6d114064207f2c21488c8489d942f
Michael Vega <ichigo509@yahoo.com>
2019-11-20 13:32:01 -0500

    changed some styling choices to better reflect screen changes

--- portfolio/client/src/Components/Night/night.js
+++ portfolio/client/src/Components/Night/night.js
@@ -3,27 +3,27 @@ import React, { Component } from "react";
 class Night extends Component {
     render() {
         return (
-            <div class="night">
-                <div class="shooting_star"></div>
-                <div class="shooting_star"></div>
-                <div class="shooting_star"></div>
-                <div class="shooting_star"></div>
-                <div class="shooting_star"></div>
-                <div class="shooting_star"></div>
-                <div class="shooting_star"></div>
-                <div class="shooting_star"></div>
-                <div class="shooting_star"></div>
-                <div class="shooting_star"></div>
-                <div class="shooting_star"></div>
-                <div class="shooting_star"></div>
-                <div class="shooting_star"></div>
-                <div class="shooting_star"></div>
-                <div class="shooting_star"></div>
-                <div class="shooting_star"></div>
-                <div class="shooting_star"></div>
-                <div class="shooting_star"></div>
-                <div class="shooting_star"></div>
-                <div class="shooting_star"></div>
+            <div className="night">
+                <div className="shooting_star"></div>
+                <div className="shooting_star"></div>
+                <div className="shooting_star"></div>
+                <div className="shooting_star"></div>
+                <div className="shooting_star"></div>
+                <div className="shooting_star"></div>
+                <div className="shooting_star"></div>
+                <div className="shooting_star"></div>
+                <div className="shooting_star"></div>
+                <div className="shooting_star"></div>
+                <div className="shooting_star"></div>
+                <div className="shooting_star"></div>
+                <div className="shooting_star"></div>
+                <div className="shooting_star"></div>
+                <div className="shooting_star"></div>
+                <div className="shooting_star"></div>
+                <div className="shooting_star"></div>
+                <div className="shooting_star"></div>
+                <div className="shooting_star"></div>
+                <div className="shooting_star"></div>
             </div>
         )
     }
